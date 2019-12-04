d1c96feac182003b6c95e031aae4c87e985be233
Ryan Rodriguez <ryanrodriguez@aclanyc.com>
2019-12-04 23:29:40 +0000

    Ryan's RPS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,70 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+$(document).ready(function() {
+  $("#shoot").click(function(){ 
+    let userInput = $("#input").val().toUpperCase();
 
-//GLOBAL VARIABLES
-/* global $ */
+    $("#userChoice").text(userInput);
+    $("#userChoice").addClass("choice")
+    
 
+    let choices = ["Rock", "Paper", "Scissors"];
 
+    let fortune = Math.random();
+    let index = Math.floor(fortune * choices.length);
+    let choice = choices[index]
 
-// DOCUMENT READY FUNCTION BELOW
+    $("#computerChoice").text(choice);
+    $("#computerChoice").addClass("comp");
 
+    $(".res").removeClass("win");
+    $(".res").removeClass("lose");
+    $(".res").removeClass("draw");
+    if(userInput === "Rock" && choice === "Rock")
+    {
+      $(".res").text("Draw!");
+      $(".res").addClass("draw");
+    }
+    else if(userInput === "ROCK" && choice === "Paper")
+    {
+      $(".res").text("You Lose");
+      $(".res").addClass("lose");
+    }
+    else if(userInput === "ROCK" && choice === "Scissors")
+    {
+      $(".res").text("You Win");
+      $(".res").addClass("win");
+    }
+    else if(userInput === "PAPER" && choice === "Rock")
+    {
+      $(".res").text("You Win");
+      $(".res").addClass("win");
+    }
+    else if(userInput === "PAPER" && choice === "Paper")
+    {
+      $(".res").text("Draw");
+      $(".res").addClass("draw");
+    }
+    else if(userInput === "PAPER" && choice === "Scissors")
+    {
+      $(".res").text("You Lose");
+      $(".res").addClass("lose");
+    }
+    else if(userInput === "SCISSORS" && choice === "Rock")
+    {
+      $(".res").text("You Lose");
+      $(".res").addClass("lose");
+    }
+    else if(userInput === "SCISSORS" && choice === "Paper")
+    {
+      $(".res").text("You Win");
+      $(".res").addClass("win");
+    }
+    else if(userInput === "SCISSORS" && choice === "Scissors")
+    {
+      $(".res").text("Draw");
+      $(".res").addClass("draw");
+    }
+
+   $("#input").val("");
+ 
+  });
+});
